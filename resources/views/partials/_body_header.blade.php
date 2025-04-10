4d5ea99bed09e8b335a0909d7da66a7d7b7d5667
SalahOummouch <s.oummouch@enim.ac.ma>
2025-04-10 04:27:42 +0100

    alerte + mail

--- resources/views/partials/_body_header.blade.php
+++ resources/views/partials/_body_header.blade.php
@@ -302,7 +302,7 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                     </svg>
-                                    <a href="{{ route('accounts.edit', Auth::user()->id) }}">Mon profil</a>
+                                    <a href="{{ route('account s.edit', ['user' => Auth::user()->id]) }}">Mon profil</a>
                                 </li>
                                 <li class="dropdown-item d-flex svg-icon">
                                     <svg class="svg-icon me-0 text-secondary" id="h-02-p" width="20"
