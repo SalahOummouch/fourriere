28d94ae361ff10cb052dd9b4a090ff3cb1d40289
SalahOummouch <s.oummouch@enim.ac.ma>
2025-04-10 04:25:08 +0100

    alerte + mail

--- resources/views/partials/_body_footer.blade.php
+++ resources/views/partials/_body_footer.blade.php
@@ -1,12 +1,7 @@
 <footer class="iq-footer">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-6">
-                <ul class="list-inline mb-0">
-                    <li class="list-inline-item"><a href="{{route('privacypolicy')}}">Privacy Policy</a></li>
-                    <li class="list-inline-item"><a href="{{route('termsofservice')}}">Terms of Use</a></li>
-                </ul>
-            </div>
+           
             <div class="col-lg-6 text-lg-end text-center mt-lg-0 mt-3">
                 <span class="mr-1">
                     © Copyright&nbsp;
