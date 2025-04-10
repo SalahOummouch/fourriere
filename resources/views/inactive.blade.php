56a3704fdcac94896ca5665a4338899345f50c1d
SalahOummouch <s.oummouch@enim.ac.ma>
2025-04-10 04:44:54 +0100

    route

--- resources/views/inactive.blade.php
+++ resources/views/inactive.blade.php
@@ -6,7 +6,7 @@
             <strong>Votre compte a été désactivé.</strong> Vous n'avez pas accès à cette page pour le moment.
         </div>
         
-        <form action="{{ route('logout') }}" method="POST">
+        <form action="/logout" method="POST">
             @csrf
             <button type="submit" class="btn btn-danger">Déconnexion</button>
         </form>
