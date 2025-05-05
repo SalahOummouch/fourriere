cc1a1e8c07e6d960d52decfdeaf04c21981b3d91
SalahOummouch <s.oummouch@enim.ac.ma>
2025-05-05 12:36:52 +0100

    new features

--- app/Console/Commands/chercherEnFouriere.php
+++ app/Console/Commands/chercherEnFouriere.php
@@ -46,8 +46,13 @@ class ChercherEnFouriere extends Command
 
             if ($doVerification) {
                 try {
+                    // Première requête à l'API 'scrape'
+                    // $response = Http::timeout(360) 
+                    //     ->post('http://77.68.95.236:5000/scrape', [
+                    //         'license_plate' => $plaque->numero_plaque
+                    //     ]);
                     $response = Http::timeout(360) 
-                        ->post('http://77.68.95.236:5000/scrape', [
+                        ->post('http://77.68.95.236:5000/scrapetest', [
                             'license_plate' => $plaque->numero_plaque
                         ]);
 
@@ -61,7 +66,34 @@ class ChercherEnFouriere extends Command
                             'archived' => $isInFourriere ?  True : FALSE , 
                             'date_recherche' => now() 
                         ]);
-                        
+
+                        // Vérifier avec l'API 'scrape1' si le véhicule est en fourrière
+                        $scrape1Response = Http::timeout(360)
+                            ->post('http://77.68.95.236:5000/scrape1', [
+                                'license_plate' => $plaque->numero_plaque
+                            ]);
+
+                        if ($scrape1Response->successful()) {
+                            $scrape1Data = $scrape1Response->json();
+                            $isInFourriereFromScrape1 = $scrape1Data['en_fouriere'];
+                            
+                            // Mise à jour supplémentaire selon la réponse de 'scrape1'
+                            if ($isInFourriereFromScrape1) {
+                                $plaque->update([
+                                    'status' => "en_fouriere",
+                                    'adresse' => $scrape1Data['adresse'] ?? $plaque->adresse,
+                                    'phone_number' => $scrape1Data['telephone'] ?? $plaque->phone_number,
+                                    'date_recherche' => now()
+                                ]);
+                            } else {
+                                // Si 'scrape1' ne retourne pas 'en_fouriere', on peut considérer qu'il n'est pas en fourrière
+                                $plaque->update([
+                                    'status' => "libre",
+                                    'date_recherche' => now()
+                                ]);
+                            }
+                        }
+
                         // Si le véhicule était hors fourrière et devient en fourrière
                         if ($isInFourriere && $plaque->status != 'en_fouriere') {
                             // Enregistrer l'alerte même si l'e-mail échoue
@@ -82,13 +114,6 @@ class ChercherEnFouriere extends Command
                                 }
                             }
                         }
-
-                        $plaque->update([
-                            'status' => $isInFourriere ? "en_fouriere" : "libre",
-                            'adresse' => $isInFourriere ? $data['adresse'] : "",
-                            'phone_number' => $isInFourriere ? $data['telephone'] : "",
-                            'date_recherche' => now() 
-                        ]);
                     } else {
                         Log::error('Échec de la requête HTTP', [
                             'plaque' => $plaque->numero_plaque,
