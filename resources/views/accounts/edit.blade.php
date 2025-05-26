9f05c1b94b168d51e26dd347d7861599560df71f
SalahOummouch <s.oummouch@enim.ac.ma>
2025-05-26 05:10:35 +0100

    new

--- resources/views/accounts/edit.blade.php
+++ resources/views/accounts/edit.blade.php
@@ -24,6 +24,13 @@
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
    </div>
 @endif
+@if(session('info'))
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        {{ session('info') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
+
 <x-app-layout>
     <div class="container-fluid">
         <div class="row">
